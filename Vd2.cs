fe35ebfe39ce5eed047300186a233f6c214b73b1
lequanghieu1709 <hieudzhihi2003@gmail.com>
2025-09-05 08:00:31 +0700

    update

--- Vd2.cs
+++ Vd2.cs
@@ -1,8 +1,3 @@
-// BadSchoolProgram.cs
-// Chương trình quản lý trường học bằng C# cực kỳ BAD CODE
-// Gồm: Sinh viên, Giáo viên, Môn học, Đăng ký, Điểm
-// Tất cả lưu bằng List<string> kiểu "id|field1|field2|..."
-
 using System;
 using System.Collections.Generic;
 
