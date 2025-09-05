7964c0adf5503035adb9b9cbf0a33a970892f91e
lequanghieu1709 <130775175+lequanghieu1709@users.noreply.github.com>
2025-09-05 08:16:45 +0700

    Update Vd2.cs

--- Vd2.cs
+++ Vd2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class BadSchoolProgram
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         List<string> students = new List<string>();
         List<string> teachers = new List<string>();
